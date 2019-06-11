3ba0a72b6d1893b67aed903ea6d662a9372ea7e8
kiet13 <kiet13.itus@gmail.com>
2019-06-11 11:11:14 +0700

    Chuc nang upload file POST method chua lam duoc

--- ProxyServer.cpp
+++ ProxyServer.cpp
@@ -9,15 +9,26 @@
 #include <vector>
 #include <string>
 #include <fstream>
+#include <sstream>
+
 #define BUFSIZE 4096
 #define PORT 8888
 
 using namespace std;
+class Client
+{
+public:
+	CSocket mainClient;
+	SOCKET connected;
+
+	Client() {  };
+	Client(const Client&) {};
+	~Client() {};
+};
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
 #endif
-
 // The one and only application object
 
 CWinApp theApp;
@@ -65,19 +76,46 @@ bool isInBlackList(char* hostname, vector<string> blacklist)
 	return false;
 }
 
+// Ref: https://stackoverflow.com/questions/2390912/checking-for-an-empty-file-in-c
+bool isEmpty(fstream& pFile)
+{
+	return pFile.peek() == fstream::traits_type::eof();
+}
 
-DWORD WINAPI threadProc(SOCKET param)
+
+//Đổi 1.1 thành 1.0 và connect
+void http_change(char request[]) {
+	//Sửa 1.1 thành 1.0
+	string temp(request);
+	int pos_http = 0;
+	pos_http = temp.find("HTTP/1.") + 7;
+	temp[pos_http] = '0';
+	
+	pos_http = temp.find("Proxy-Connection:") + 18;
+	// xoa dong keep-alive
+	temp.erase(temp.begin() + pos_http, temp.begin() + pos_http + 10);
+	// thay vao do la close
+	temp.insert(pos_http, "close");
+
+	strcpy(request, temp.c_str());
+	return;
+}
+
+DWORD WINAPI threadProc(void* param)
 {
 	// Tao mot socket de proxy de nhan request tu client
+	Client* client = (Client*)param;
 	CSocket proxyRecv;
 	proxyRecv.Create(PORT, SOCK_STREAM, NULL);
-	proxyRecv.Attach(param);
+	proxyRecv.Attach(client->connected);
 
 	// Proxy nhan request tu client va luu vao clientRequest
 	char clientRequest[BUFSIZE + 1];
 	memset((char*)clientRequest, 0, BUFSIZE + 1);
-	proxyRecv.Receive((char*)clientRequest, BUFSIZE, 0);
 
+	// proxy nhận phần header
+	int readBytes = proxyRecv.Receive((char*)clientRequest, BUFSIZE, 0);
+	//http_change(clientRequest);
 	if (clientRequest == NULL || strcmp(clientRequest, "") == 0)
 	{
 		proxyRecv.Close();
@@ -98,6 +136,16 @@ DWORD WINAPI threadProc(SOCKET param)
 
 	// Tach URL
 	char* URL = strtok(NULL, " ");
+	vector<string> path;
+	int count = 0;
+	string strURL(URL);
+	stringstream input_stringstream(strURL);
+	path.resize(count + 1);
+	while (getline(input_stringstream, path[count], '/'))
+	{
+		count++;
+		path.resize(count + 1);
+	}
 
 	// Tach hostname
 	char* hostname = strtok(URL, "/");
@@ -141,8 +189,27 @@ DWORD WINAPI threadProc(SOCKET param)
 	// Lay dia chi IP tu host name
 	char* ip = get_ip(hostname);
 
-	// Xuat ra cac request ma proxy server nhan duoc
-	cout << clientRequest << endl;
+	string tempURL(ip);
+	path[0] += "//";
+	tempURL = path[0] + tempURL;
+	for (int i = 3; i < count; i++)
+		tempURL = tempURL + "/" + path[i];
+
+	// Kiem tra xem url co subpath hay khong
+	string tempRequest(clientRequest);
+	int sizeHostName = strlen(hostname);
+	if (!((path.size() > 3 && path[3] == "") || path.size() <= 3)) // Neu khong co subpath
+	{
+		// Xoa domain name trong clientRequest
+		int firstIndex = 0;
+		if (strcmp(method, "GET") == 0)
+			firstIndex = 4;
+		else if (strcmp(method, "POST") == 0)
+			firstIndex = 5;
+
+		tempRequest.erase(tempRequest.begin() + firstIndex, tempRequest.begin() + firstIndex + sizeHostName + 7);
+	}
+	cout << tempRequest << endl;
 
 	// Tao mot client ao tai proxy de gui request da nhan tu client that toi server
 	CSocket proxyClient;
@@ -152,8 +219,87 @@ DWORD WINAPI threadProc(SOCKET param)
 		perror("Could not connect");
 		exit(1);
 	}
-	proxyClient.Send((char*)clientRequest, BUFSIZE, 0);
+	proxyClient.Send((char*)tempRequest.c_str(), BUFSIZE, 0);
+	
+	if (strcmp(method, "POST") == 0)
+	{
+		// Doc content-length
+		// Doc dong thu 4
+		string lines(clientRequest);
+		stringstream s(lines);
 
+		string contentLength;
+
+		while (1)
+		{
+			getline(s, contentLength, '\n');
+			if (contentLength.find("Content-Length:") != string::npos)
+			{
+				s.str(contentLength);
+				getline(s, contentLength, ' ');
+				getline(s, contentLength, '\r');
+				break;
+			}
+		}
+		// Doc content-type
+		string contentType;
+		s.str(lines);
+		while (1)
+		{
+			getline(s, contentType, '\n');
+			if (contentType.find("Content-Type:") != string::npos)
+			{
+				s.str(contentType);
+				getline(s, contentType, ' ');
+				getline(s, contentType, ';');
+				break;
+			}
+		}
+
+		if (contentType != "application/x-www-form-urlencoded")
+		{
+
+			int contentLen = stoi(contentLength);
+			// Neu nhu phan body cua POST request qua lon thi thuc hien luu vao file
+			if (contentLen > BUFSIZE)
+			{
+				while (1)
+				{
+					proxyRecv.Accept(client->mainClient);
+					unsigned char chunkDataInFile[BUFSIZE + 1];
+					memset((char*)chunkDataInFile, 0, BUFSIZE + 1);
+					readBytes = proxyRecv.Receive((unsigned char*)chunkDataInFile, BUFSIZE, 0);
+					if (readBytes <= 0)
+						break;
+					proxyClient.Send((unsigned char*)chunkDataInFile, readBytes, 0);
+				}
+			}
+			else
+			{
+				unsigned char bodyRequest[BUFSIZE + 1];
+				memset((char*)bodyRequest, 0, BUFSIZE + 1);
+				proxyRecv.Accept(client->mainClient);
+				proxyRecv.Receive((unsigned char*)bodyRequest, contentLen, 0);
+
+				unsigned char fullRequest[BUFSIZE + 1];
+				memset((char*)fullRequest, 0, BUFSIZE + 1);
+
+				int sizeHeader = tempRequest.size();
+				for (int i = 0; i < sizeHeader; i++)
+				{
+					fullRequest[i] = tempRequest[i];
+				}
+				for (int i = 0; i < contentLen; i++)
+				{
+					fullRequest[sizeHeader + i] = bodyRequest[i];
+				}
+				
+				proxyClient.Send((unsigned char*)fullRequest, sizeHeader + contentLen, 0);
+			}
+		}
+
+	}
+	
 	while (1)
 	{
 		// Nhan du lieu tu web server
@@ -204,10 +350,10 @@ int main()
 				while (1) //server khong dung luon nhan ket noi tu client
 				{
 					// Moi vong lap xu ly mot request tu client
-					CSocket mainClient;
-					proxyServer.Accept(mainClient);
-					SOCKET connected = mainClient.Detach();
-					CreateThread(0, 0, (LPTHREAD_START_ROUTINE)&threadProc, (LPVOID)connected, 0, 0);
+					Client* user = new Client;
+					proxyServer.Accept(user->mainClient);
+					user->connected = user->mainClient.Detach();
+					CreateThread(0, 0, (LPTHREAD_START_ROUTINE)&threadProc, user, 0, 0);
 				}
 			}
         }
